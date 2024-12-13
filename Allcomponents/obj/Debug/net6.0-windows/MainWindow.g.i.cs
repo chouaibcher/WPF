91a2d191c4933ff5d9196ab27b43c5abedc9e8cd
CHERIBET CHERIF CHOUAIB <cherifchouaib.cheribet@univ-skikda.dz>
2024-12-13 18:04:20 +0100

    .

--- Allcomponents/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Allcomponents/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace AllComponents {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Allcomponents;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Allcomponents;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
