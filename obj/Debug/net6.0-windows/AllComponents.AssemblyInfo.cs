036cb7d2b7336f3f6800b82722cb81792c5adb81
CHERIBET CHERIF CHOUAIB <cherifchouaib.cheribet@univ-skikda.dz>
2024-12-13 14:13:33 +0100

    .

--- obj/Debug/net6.0-windows/AllComponents.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/AllComponents.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllComponents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a69fed48a1365b93422c6b0515867269f2d8d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllComponents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
